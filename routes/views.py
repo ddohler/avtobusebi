7f3e523b5b2401b66b9d2e226fe14d12ef522789
ddohler <ddohler@ubuntu-dev.(none)>
2011-04-25 17:29:32 +0400

    Made switching projections for deployment easier

--- routes/views.py
+++ routes/views.py
@@ -19,17 +19,15 @@ def main(request):
     # Using actual Tbilisi data so we don't land somewhere
     # random if something bad happens...although
     # that might be a good indication something broke.
-    dummy_geom = fromstr('LINESTRING (4978262.6790810525417328 '+\
-                                      '5118097.6463875807821751, '+\
-                                      '4978283.0049614785239100 '+\
-                                      '5118077.6137111270800233)', srid=3857)
-                    
+    dummy_geom = fromstr('LINESTRING (4978262.6790810525417328 5118097.6463875807821751,4978283.0049614785239100 5118077.6137111270800233)', srid=3857)
+    #dummy_geom = fromstr('LINESTRING (44.792998 41.709981,44.82421875 41.70624114327587',srid=4326)
     route_layers.append(InfoLayer([[dummy_geom, 'draw']],
                                   {'name':'draw'}))
     # construct olwidget map 
     olmap = Map(vector_layers=route_layers,options={'map_div_style':{'background-color': 'white'},
             'map_options': {
                 'controls': ['Navigation','PanZoom'], 'display_projection':'EPSG:3857'}},
+                #'controls': ['Navigation','PanZoom'], 'display_projection':'EPSG:4326'}},
         template='olwidget-custom.html')
     # render
     return render_to_response('main.html', {'olmap':olmap,'routes':all_routes,})
